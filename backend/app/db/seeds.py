5dad834390e32b29b9e271844cb2e8588c2f6681
Oliver Herman <oliver.herman@tufts.edu>
2023-02-27 03:10:16 +0000

    initial commit

--- backend/app/db/seeds.py
+++ backend/app/db/seeds.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import sys
 import flask_sqlalchemy 
 
+
 sys.path.insert(0, str(Path(__file__).parent.parent.parent.parent))
 
 # Use models to seed database with 100 users, 100 items, and 100 comments.
