5c51167d61c262f18c2de7ac09c296b5e159859e
Oliver Herman <oliver.herman@tufts.edu>
2023-03-03 18:14:51 -0500

    Update seeds.py

--- backend/app/db/seeds.py
+++ backend/app/db/seeds.py
@@ -1,46 +1,45 @@
 from pathlib import Path
 import sys
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+import random
+import string
 
 
-sys.path.insert(0, str(Path(__file__).parent.parent.parent.parent))
 
-# Use models to seed database with 100 users, 100 items, and 100 comments.
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
-from sqlalchemy.orm import Session
-from backend.app.models.domain import users, items, comments 
 
-def seed_db(db: Session, users: int = 100, items: int = 100, comments: int = 100):
+
+database_url = os.environ['DATABASE_URL'].replace("postgres://", "postgresql://")
+
+# declare sql statements
+user_insert_statement = text("""INSERT INTO users(username, email, salt, bio, hashed_password) VALUES(:username, :email, :salt, :bio, :hashed_password) ON CONFLICT DO NOTHING""")
+select_last_user_id = text("""SELECT * FROM users ORDER BY id DESC LIMIT 1""")
+item_statement = text("""INSERT INTO items(slug, title, description, seller_id) VALUES(:slug, :title, :description, :seller_id) ON CONFLICT DO NOTHING""")
+select_last_item_id = text("""SELECT * FROM items ORDER BY id DESC LIMIT 1""")
+comment_statement = text("""INSERT INTO comments(body, seller_id, item_id) VALUES(:body, :seller_id, :item_id) ON CONFLICT DO NOTHING""")
+
+# create engine
+engine = create_engine(database_url, echo=False)
+
+with engine.connect() as con:
+  for i in range(100):
     
-    # Seed users
-    for i in range(users):
-        user = users.UserCreate(
-            username=f"user{i}",
-            email=f"user{i}@email.com",
-            password="password",
-            is_active=True,
-        )
-
-        db.add(user)
-
-    # Seed items
-    for i in range(items):
-        item = items.ItemCreate(
-            title=f"Item {i}",
-            description="Description",
-            price=100,
-            owner_id=1,
-        )
-
-        db.add(item)
-
-    # Seed comments
-    for i in range(comments):
-        comment = comments.CommentCreate(
-            body=f"Comment {i}",
-            seller_id=1,
-        )
-
-        db.add(comment)
-
-    db.commit()
-    db.refresh(db)
+    random_username = f'user{i}'
+    user = {'username': random_username, 'email':f'{random_username}@mail.com', 'salt': 'abc', 'bio': 'bio', 'hashed_password':'12345689'}
+    con.execute(user_insert_statement, **user)
+    
+    result = con.execute(select_last_user_id)
+    for row in result:
+      generated_user_id = row['id']
+    
+    item = {'slug':f'slug-{random_username}', 'title':f'title{i}','description':f'desc{i}', 'seller_id':generated_user_id}
+    con.execute(item_statement, **item)
+    
+    item_result = con.execute(select_last_item_id)
+    for row in item_result:
+      generated_item_id = row['id']
+    comment = {'body': f'comment{i}', 'seller_id': generated_user_id, 'item_id': generated_item_id}
+    con.execute(comment_statement, **comment)
