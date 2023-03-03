e76cea79ea9e9ad6a1198763762df556b448037e
WilcoApp <hello@wilco.gg>
2023-03-03 23:17:04 +0000

    Hooks of Change

--- backend/app/models/domain/users.py
+++ backend/app/models/domain/users.py
@@ -1,8 +1,4 @@
-from pathlib import Path
-import sys
 from typing import Optional
-
-sys.path.insert(0, str(Path(__file__).parent.parent.parent.parent))
 
 from app.models.common import DateTimeModelMixin, IDModelMixin
 from app.models.domain.rwmodel import RWModel
